b7ac43e7cc548b66afd5ecad082c34cc2fa3cbdd
KentonK <72780911+KentonK@users.noreply.github.com>
2020-11-18 11:12:52 -0700

    Added Project 3

---
+++ src/Project3/Main.java
@@ -0,0 +1,12 @@
+package Project3;
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args){
+        JFrame window = new JFrame("ticktacktoe");
+        window.setBounds(100, 100, 300, 300);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().add(new Game());
+        window.setVisible(true);
+
+    }
+}
