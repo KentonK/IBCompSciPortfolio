f59e92e6d5cb95a7f7e804a4988439b64a4ec8c2
KentonK <72780911+KentonK@users.noreply.github.com>
2020-11-18 11:11:21 -0700

    Added Project 3

--- src/Project3/Game.java
+++ src/Project3/Game.java
@@ -6,24 +6,37 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Game extends JPanel {
-    private JButton[] buttons= new JButton[9];
+    private JButton[] buttons = new JButton[9];
+    public Boolean turn = true;
 
-    public Game(){
-        this.setLayout(new GridLayout(3,3));
+    public Game() {
+        this.setLayout(new GridLayout(3, 3));
         for (int i = 0; i < 9; i++) {
-            buttons[i]=new JButton();
-            buttons[i].setText("");
-
+            buttons[i] = new JButton();
+            buttons[i].setText("_");
+            buttons[i].addActionListener(new ButtonListener());
             this.add(buttons[i]);
         }
     }
-    private class ButtonListener implements ActionListener{
-        private String turn="X";
+
+    private class ButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             JButton buttonClicked = (JButton) e.getSource();
-            buttonClicked.setText(turn);
-            turn=(turn=="X")?"O":"X";
+            buttonClicked.setText(turn == true ? "X" : "O");
+            turn = turn == true ? false : true;
+            
+        }
+        public boolean win(Button[] JB){
+            boolean win=false;
+            for (int i = 0; i < 3; i++) {
+                if(JB[i]==JB[i+3]&&JB[i+3]==JB[i+6]){win=true;}
+            }
+            for (int i = 0; i < 9; i+=3){
+                if (JB[i] == JB[i + 1] && JB[i + 1] == JB[i+2]){win=true;}
+            }
+            if((JB[0]==JB[4]&&JB[4]==JB[8])||(JB[2]==JB[4]&&JB[4]==JB[6])){win=true;}
+            return win;
         }
     }
 }
---
+++ src/Project3/Win.java
@@ -0,0 +1,7 @@
+package Project3;
+
+public class Win {
+    public check(){
+
+    }
+}
